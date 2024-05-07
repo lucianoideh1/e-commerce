e2e4f068b1b6e1e1d1ada29f2575f5e9e8b1fd16
lucianoideh <vazquezluciano5@gmail.com>
2024-05-07 16:31:43 -0300

    added error page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import Root from './routes/root'
-
+import ErrorPage from './error-page'
 const router = createBrowserRouter([
   {
     path:'/',
     element:<Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>
   },
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,11 +3,11 @@ export default function Root(){
         <div id='index-site' className="w-screen">
             <div id='nav-bar'className="flex gap-6 items-center justify-center w-full p-2 bg-slate-200	 ">
                 <h1 className="text-2xl text-gray-900 font-extrabold">title</h1>
-                <ul className="flex gap-1 text-l">
-                    <li className="p-1">about</li>
-                    <li className="p-1">shop</li>
-                    <li className="p-1">categories</li>
-                    <li className="p-1">cart</li>
+                <ul className="flex gap-1 text-l font-medium">
+                    <li className="p-1"> <a href="about">about</a></li>
+                    <li className="p-1"><a href="shop">shop</a></li>
+                    <li className="p-1"><a href="categories">categories</a></li>
+                    <li className="p-1"><a href="cart">cart</a></li>
                 </ul>
             </div>
         <div className="details"></div>
