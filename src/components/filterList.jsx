f45159514b24662bd8e144d70124c91f48bd28f3
lucianoideh <vazquezluciano5@gmail.com>
2024-05-14 12:46:49 -0300

    added base style to shop children

--- src/components/filterList.jsx
+++ src/components/filterList.jsx
@@ -1,6 +1,9 @@
 export const FilterList = () => {
   return (
-    <div>filterList</div>
+    <div className="p-2 bg-slate-200 border-2 border-gray-500 m-2 ">
+        <h3 className="border-b-2 border-slate-600 text-xl font-bold">filterList</h3>
+
+    </div>
   )
 }
 
