48d36d82262c30e101a41fc239111f265a0f9560
lucianoideh <vazquezluciano5@gmail.com>
2024-05-14 13:40:40 -0300

    add grid layout

--- src/components/itemCollection.jsx
+++ src/components/itemCollection.jsx
@@ -1,6 +1,8 @@
 export const ItemCollection = ()  => {
     return(
-        <div className="p-2 bg-slate-200 border-2 border-gray-500 m-2  text-xl font-bold">ItemCollection</div>
+        <div className="p-2 bg-slate-200 border-2 border-gray-500 m-2  text-xl font-bold">
+            <h3>ItemCollection</h3>
+        </div>
     )
 }
 
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -15,10 +15,10 @@ const items = useLoaderData()
 return(
     <>
     {items.length ? (
-        <div>            
+        <div id="shop" className="grid grid-cols-4 p-8">            
         <FilterList></FilterList>
-        <ItemCollection></ItemCollection>
-        <div className="item-two flex flex-wrap justify-center">
+        {/* <ItemCollection></ItemCollection> */}
+        <div className="item-two flex flex-wrap justify-center col-start-2 col-end-5">
         {items.map((item)=>(
             <div key={item.id} className="p-2 bg-slate-200 border-2 border-gray-500 m-2 max-w-48">
             <p className="font-bold text-xl">{item.title}</p>
