555839c55ac2bca8c36fa0a0940208364cabb770
lucianoideh <vazquezluciano5@gmail.com>
2024-05-17 11:13:08 -0300

    add style to cards and collection

--- src/components/itemCollection.jsx
+++ src/components/itemCollection.jsx
@@ -11,15 +11,15 @@ useEffect(() => {
     return(
         <div id="collection" className="col-start-2 col-end-7 p-2 bg-slate-200 shadow-md shadow-slate-600 m-2  text-lg font-semibold flex flex-col items-center">
             <h3>ItemCollection</h3>
-                <ul className="grid grid-cols-4 min-w-full gap-4">
+                <ul className="flex flex-wrap gap-4  min-w-full">
                     {loading ? null : (
                             state.map((i) => (
-                            <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400 max-w-48"> 
+                            <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400 max-w-48 max-h-72 overflow-y-hidden hover:scale-105 hover:cursor-pointer hover:shadow-blue-500 transition-all duration-500"> 
                             <div className="flex flex-col leading-3">
-                            <img src={i.image} alt="item" />
+                            <img src={i.image} alt="item" className='object-cover max-h-40' />
                             <p className="font-medium text-lg">${i.price}</p>
                             <p className="text-gray-700 text-base">{i.category}</p>
-                            <p className="font-bold text-lg">{i.title}</p>
+                            <p className="font-bold text-base">{i.title}</p>
                          </div></li>))
                     )}
                 </ul>
