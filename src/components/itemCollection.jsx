ca69b8438dd98abfb605453cb4b000b3c95ba8e5
lucianoideh <vazquezluciano5@gmail.com>
2024-05-17 10:38:44 -0300

    item collection styles

--- src/components/itemCollection.jsx
+++ src/components/itemCollection.jsx
@@ -3,15 +3,15 @@ export const ItemCollection = ({item})  => {
     return(
         <div id="collection" className="col-start-2 col-end-7 p-2 bg-slate-200 shadow-md shadow-slate-600 m-2  text-lg font-semibold flex flex-col items-center">
             <h3>ItemCollection</h3>
-            <ul className="grid grid-cols-10 min-w-full gap-4">
+            <ul className="grid grid-cols-6 min-w-full gap-4">
             {item.map((i) => (
-            <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400  m-2 max-w-48"> 
-            <div >
-            <p className="font-bold text-xl">{i.title}</p>
-            <p className="text-gray-700 text-base">{i.category}</p>
-            <p className="font-medium text-lg">${i.price}</p>
-            {/* <p><span>rating ({item.rating}/5) - {item.count}</span></p> */}
+            <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400  min-w-48 max-w-48"> 
+            <div className="flex flex-col leading-3">
             <img src={i.image} alt="item" />
+            <p className="font-medium text-lg">${i.price}</p>
+            <p className="text-gray-700 text-base">{i.category}</p>
+            {/* <p><span>rating ({item.rating}/5) - {item.count}</span></p> */}
+            <p className="font-bold text-lg">{i.title}</p>
         </div></li>))}
             </ul>
         </div>
