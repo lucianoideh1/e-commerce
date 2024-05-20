55b6dd1da821f7258d2cc09551e4c3008f7f19b3
lucianoideh <vazquezluciano5@gmail.com>
2024-05-20 14:27:54 -0300

    removed bg and title in collection

--- src/components/itemCollection.jsx
+++ src/components/itemCollection.jsx
@@ -10,9 +10,8 @@ useEffect(() => {
     alert()
 },[state, loading])
     return(
-        <div id="collection" className="col-start-2 col-end-7 p-2 bg-slate-200 shadow-md shadow-slate-600 m-2  text-lg font-semibold flex flex-col items-center">
-            <h3>ItemCollection</h3>
-                <ul className="flex flex-wrap gap-4  min-w-full">
+        <div id="collection" className="col-start-2 col-end-7 p-2  shadow-slate-600 m-2  text-lg font-semibold flex flex-col items-center">
+                <ul className="flex flex-wrap gap-4  justify-center min-w-full">
                     {loading ? null : (
                             state.map((i) => (
                                     <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400 max-w-48 max-h-72 overflow-y-hidden hover:scale-105 hover:cursor-pointer hover:shadow-slate-900 transition-all duration-500"> 
