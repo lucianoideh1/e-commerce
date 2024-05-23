f429d7a9b846e62a296a72d190ee38f3db547f2e
lucianoideh <vazquezluciano5@gmail.com>
2024-05-23 15:42:17 -0300

    display cart conditionally and display price and quantity

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -5,11 +5,19 @@ export const Cart = () => {
     const {cart,setCart} = context
     console.log(context)
     console.log(cart)
-    return(
-        <section id="cart">
-            cart
-            {cart.map(i => <div key={i.product_name}>{i.product_name}</div>)}
-        </section>
-    )
+
+    if(cart.length === 0){
+        return(<h1 className="text-6xl font-bold">Cart is currently empty</h1>)
+    }
+    else if(cart.length > 0){
+        return(
+            <section id="cart_products">
+           {cart.map((i) => (
+           <div key={i.product_name}>
+            {i.product_name} - x{i.quantity} - total: ${(i.quantity * i.product_price)}
+            </div>))} 
+            </section>
+        )
+    }
 }
 
