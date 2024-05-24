f03f9b3fb0dae27377a1506474311806cec7acba
lucianoideh <vazquezluciano5@gmail.com>
2024-05-24 13:59:57 -0300

    implemented payout section and style

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -11,13 +11,21 @@ export const Cart = () => {
     }
     else if(cart.length > 0){
         return(
-            <section id="cart_products">
+            <section id="cart">
+
+            
+            <section id="cart_products" className="mb-8">
                 <ul>
            {cart.map((i) => (
            <li key={i.product_name} className="border-2 border-gray-800 m-2 py-2 px-4 rounded-md">
             {i.product_name} - x{i.quantity} - total: ${(i.quantity * i.product_price)}
             </li>))} 
             </ul>
+            </section>
+            <section id="payout" className="flex flex-col gap-4 font-semibold text-2xl">
+                <p className="flex items-center justify-center">total: x</p>
+                <button className="bg-slate-900 text-slate-100 py-2 px-4 font-medium text-2xl rounded-lg flex items-center uppercase justify-center hover:bg-slate-700 transition-colors duration-300">payout</button>
+            </section>
             </section>
         )
     }
