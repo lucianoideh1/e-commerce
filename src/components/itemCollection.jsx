30628d5fab61c091de6615756b75d3643749c124
lucianoideh <vazquezluciano5@gmail.com>
2024-05-28 11:49:34 -0300

    add a mobile navbar

--- src/components/itemCollection.jsx
+++ src/components/itemCollection.jsx
@@ -10,7 +10,7 @@ export const ItemCollection = ({ state,loading})  => {
                 <ul className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-6">
                     {loading ? null : (
                             state.map((i) => (
-                                    <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400 max-w-48 max-h-72 overflow-hidden hover:scale-105 hover:cursor-pointer hover:shadow-slate-900 transition-all duration-500"> 
+                                    <li key={i.title} className="rounded-sm p-2 bg-slate-300 shadow-md shadow-slate-400 max-w-48 max-h-72 overflow-hidden lg:hover:scale-105 hover:cursor-pointer hover:shadow-slate-900 transition-all duration-500"> 
                                 <Link to={`/products/${i.id}`}>
                                    <Card props={{image:i.image,price:i.price,category:i.category,title:i.title}}></Card>
                                 </Link>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -14,6 +14,14 @@ const [cart, setCart] = useState([])
                     <li className="p-1"><Link to={"/cart"}>cart({cart.length})</Link></li>
                 </ul>
             </section>
+            <section id="mobile-nav-bar" className="fixed lg:hidden bottom-0 min-w-full">
+                <ul className=" flex justify-around bg-cyan-600">
+                    <li className="p-4 "><Link className=" text-slate-100 hover:text-gray-800" to={'/about'}>Home</Link></li>
+                    <li className="p-4 "><Link className=" text-slate-100 hover:text-gray-800" to={'/about'}>About</Link></li>
+                    <li className="p-4"><Link className=" text-slate-100 hover:text-gray-800" to={'/shop'}>Shop</Link></li>
+                    <li className="p-4"><Link className=" text-slate-100 hover:text-gray-800" to={'/cart'}>Cart({cart.length})</Link></li>
+                </ul>
+            </section>
             <main id="details" className="flex justify-center">
             <Outlet context={{cart,setCart}}></Outlet>
             </main>
