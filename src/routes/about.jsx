09d446b43da9a843620529371ca64210826e2ef5
lucianoideh <vazquezluciano5@gmail.com>
2024-05-29 13:49:31 -0300

    updated style in about section

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -3,13 +3,13 @@ export const About = () => {
     return(
         <section id="about" className="p-4">
             <Link to={'/'}>Go back</Link>
-        <article>
-        <p className="text-2xl font-semibold">We are</p>    
-        <p>A company oriented in technology for e-commerce. Our purpose is to democratize trade to transform millions of lifes from people from all parts of the region.</p>
+        <article className='mb-12 mt-12'>
+        <p className="text-2xl font-semibold bg-gray-900 text-gray-100 p-2">We are</p>    
+        <p className='max-w-lg'>A company oriented in technology for e-commerce. Our purpose is to democratize trade to transform millions of lifes from people from all parts of the region.</p>
         </article>
         <article>
-         <p className="text-2xl font-semibold">We Do</p>
-         <p>We develop techonologic products that allow users to buy, sell, announce, ship, and pay through internet and in a easy way, safe and efficient.</p>  
+         <p className="text-2xl font-semibold bg-gray-900 text-gray-100 p-2">We Do</p>
+         <p className='max-w-lg'>Develop techonologic products that allow users to buy, sell, announce, ship, and pay through internet and in a easy way, safe and efficient.</p>  
         </article>
         </section>
     )
